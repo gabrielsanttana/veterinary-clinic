47a5d01650dae2d899a08c12efd8f0c4dfadbf2e
ticollapse <vvpll@outlook.com>
2020-05-14 21:36:26 -0300

    Class Client - Metodos vazios e atributos

--- src/resistencia/Class/Client.java
+++ src/resistencia/Class/Client.java
@@ -2,9 +2,21 @@ package resistencia;
 
 public class Client {
     int id;
-
-    public void getId() {
-
+    
+    private String name;
+    private String adress;
+    private String tel;
+    private double cep;
+    private String email;
+    
+    
+    public int Reg_Cli() {
+    	return 0;
     }
+    
+    public String Con_Cli() {
+    	return "";
+    }
+    
 
 }
