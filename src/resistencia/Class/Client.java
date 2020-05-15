ca2b456564846ca8e86c8fd2598d051fd092ce2d
ticollapse <vvpll@outlook.com>
2020-05-14 22:15:03 -0300

    remove get e set id

--- src/resistencia/Class/Client.java
+++ src/resistencia/Class/Client.java
@@ -29,14 +29,6 @@ public class Client {
     	return "";
     }
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
 	}
