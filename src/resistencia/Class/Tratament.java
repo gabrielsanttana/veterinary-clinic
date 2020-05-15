89974e21db361f31d9478745e646610df0707483
LucasSalu <samuel_6000@outlook.com>
2020-05-14 22:33:16 -0300

    Class Tratament

---
+++ src/resistencia/Class/Tratament.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+
+public class Tratament {
+	
+	private Date datStart;
+	private Date dateEnd;
+	
+	
+	
+	private Tratament(Date datStart, Date dateEnd) {
+		super();
+		this.datStart = datStart;
+		this.dateEnd = dateEnd;
+	}
+	
+	public Date getDatStart() {
+		return datStart;
+	}
+	public void setDatStart(Date datStart) {
+		this.datStart = datStart;
+	}
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
+	}
+	
+	public String listAppointment() {
+	
+	}
+	public String appointmentTratament() {
+		
+	}
+	public int registerTratament() {
+		
+	}
+		
+
+}
