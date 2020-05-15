907e48f05c0cd44900b8eef00d8619ff5b0210ed
231396 <a231396@dac.unicamp.br>
2020-05-14 22:00:01 -0300

    Consulta e Date

---
+++ src/resistencia/Class/Consulta.java
@@ -0,0 +1,39 @@
+package resistencia.Class;
+
+public class Consulta 
+{
+	private Date consultaDate;
+	private String historico;
+	
+	public String getHistorico() {
+		return historico;
+	}
+
+	public void setHistorico(String historico) {
+		this.historico = historico;
+	}
+
+	public Date getConsultaDate() {
+		return consultaDate;
+	}
+
+	public void setConsultaDate(Date consultaDate) {
+		this.consultaDate = consultaDate;
+	}		
+	
+	
+	public String Lista_Consulta()
+	{
+		return "";		
+	}
+	
+	public String Ver_Consulta() 
+	{
+		return "";		
+	}
+	
+	public int Registra_Consulta() 
+	{
+		return 0;		
+	}
+}
---
+++ src/resistencia/Class/Date.java
@@ -0,0 +1,32 @@
+package resistencia.Class;
+
+public class Date 
+{
+	int day, month, year;
+	
+	public int GetDay () 
+	{
+		return day;		
+	}
+	public int GetMonth () 
+	{
+		return day;		
+	}	
+	public int GetYear () 
+	{
+		return day;		
+	}
+	
+	public void SetDay (int day) 
+	{
+		this.day = day;
+	}
+	public void SetMonth (int month) 
+	{
+		this.month = month;		
+	}	
+	public void SetYear (int year) 
+	{
+		this.year = year;
+	}	
+}
