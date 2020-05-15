27ff5614bf0e906742b4bf86dd0e32f6c47dda33
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-05-14 21:45:14 -0300

    Create class

---
+++ src/resistencia/Class/Specie.java
@@ -0,0 +1,22 @@
+package resistencia;
+
+public class Specie {
+	private String spacie_name;
+	
+    public void Specie() {
+    	
+    }
+	
+    public void Specie(String spacie_name) {
+    	this.spacie_name = spacie_name;
+    }
+    
+    public String getSpacieName() {
+    	return this.spacie_name;
+    }
+    
+    public void setSpacieName(String spacie_name) {
+    	this.spacie_name = spacie_name;
+    }
+
+}
