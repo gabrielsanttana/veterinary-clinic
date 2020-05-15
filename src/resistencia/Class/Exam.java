689f2e464dc2f0d1fba3dc329c47f55c10cb1a20
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-05-14 21:55:10 -0300

    Create class

---
+++ src/resistencia/Class/Exam.java
@@ -0,0 +1,22 @@
+package resistencia;
+
+public class Exam {
+    String exam_desc;
+
+
+    public void Exam() {
+
+    }
+
+    public void Exam(String exam_desc) {
+    	this.exam_desc = exam_desc;
+    }
+
+    public String getExamDesc() {
+    	return this.exam_desc;
+    }
+
+    public void setExamDesc(String exam_desc) {
+    	this.exam_desc = exam_desc;
+    }
+}
