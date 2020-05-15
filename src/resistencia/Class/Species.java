a89a88e047f5c3a60edf9a414ef3d8f9cfe91b8d
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-05-14 22:17:09 -0300

    Fix names

---
+++ src/resistencia/Class/Species.java
@@ -0,0 +1,22 @@
+package resistencia;
+
+public class Species {
+    private String spacieName;
+
+    public Species() {
+
+    }
+
+    public Species(String spacieName) {
+        this.spacieName = spacieName;
+    }
+
+    public String getSpacieName() {
+        return this.spacieName;
+    }
+
+    public void setSpacieName(String spacieName) {
+        this.spacieName = spacieName;
+    }
+
+}
