0fffac79abf258785a9dc902d2fa0e7534635a03
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-21 20:34:42 -0300

    Rename 'AnimalVet' class to 'Veterinary' and its respective instances

---
+++ src/VeterinaryClinic/model/Veterinary.java
@@ -0,0 +1,37 @@
+package resistencia.model;
+
+public class Veterinary {
+    private String veterinaryName;
+    private String veterinaryAddress;
+    private String veterinaryPhone;
+
+    public Veterinary(String veterinaryName, String veterinaryAddress, String veterinaryPhone) {
+        this.veterinaryName = veterinaryName;
+        this.veterinaryAddress = veterinaryAddress;
+        this.veterinaryPhone = veterinaryPhone;
+    }
+
+    public String getVeterinaryName() {
+        return this.veterinaryName;
+    }
+
+    public void setVeterinaryName(String veterinaryName) {
+        this.veterinaryName = veterinaryName;
+    }
+
+    public String getVeterinaryAddress() {
+        return this.veterinaryAddress;
+    }
+
+    public void setVeterinaryAddress(String veterinaryAddress) {
+        this.veterinaryAddress = veterinaryAddress;
+    }
+
+    public String getVeterinaryPhone() {
+        return this.veterinaryPhone;
+    }
+
+    public void setVeterinaryPhone(String veterinaryPhone) {
+        this.veterinaryPhone = veterinaryPhone;
+    }
+}
