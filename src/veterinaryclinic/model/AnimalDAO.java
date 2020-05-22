3683b37c121366646625e3dbda3e82e1fd36bb31
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-21 22:17:44 -0300

    Create AnimalDAO model

--- src/veterinaryclinic/model/AnimalDAO.java
+++ src/veterinaryclinic/model/AnimalDAO.java
@@ -1,3 +1,48 @@
-public class AnimalDAO {
-  
+package model;
+
+import java.util.Observable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalDAO extends Observable {
+  private static AnimalDAO instance;
+  private List<Animal> animals;
+  private int id;
+
+  private AnimalDAO() {
+    animals = new ArrayList<Animal>();
+    id = 0;
+  }
+
+  public static AnimalDAO getInstance() {
+    if(instance == null) {
+      instance = new AnimalDAO();
+    }
+
+    return instance;
+  }
+
+  public void addAnimal(String nome, int age, String sex, Treatment[] treatments, Species[] species) {
+    Animal animal = new Animal(nome, age, sex, treatments, species);
+    id++;
+    animals.add(animal);
+    setChanged();
+    notifyObservers(animal);
+  }
+
+  public List<Animal> getAllAnimals() {
+    return animals;
+  }
+
+  public Animal getAnimalById(int id) {
+    for(Animal animal : animals) {
+      if(animal.getId() == id) {
+        return animal;
+      }
+    }
+  }
+
+  public void deleteAnimal(Animal animal) {
+    animals.remove(animal);
+  }
 }
