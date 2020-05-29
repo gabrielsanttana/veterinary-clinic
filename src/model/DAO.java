9a4548f3961947b8b060e102c4895b83af6dfd82
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-05-28 21:38:51 -0300

    Ajust sqlite

--- src/model/DAO.java
+++ src/model/DAO.java
@@ -1,14 +1,46 @@
+package model;
+
 import java.sql.*;
 
 public abstract class DAO {
+	
+	private static Connection con;
 
-    private static void connect() {
+    public static Connection getConnection() {
+    	if (con != null) {
+    		return con;
+    	}
+    	
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:banco.db")) {
 
             System.out.println("Conexão realizada !!!!");
-
+            
+            con = connection;
+            
+            return connection;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        
+        return null;
+    }
+
+    protected ResultSet getResultSet(String query) {
+        Statement s;
+        ResultSet rs = null;
+        try {
+            s = (Statement) con.createStatement();
+            s.executeQuery(query);
+            rs = s.getResultSet();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+    
+    protected int executeUpdate(PreparedStatement queryStatement) throws SQLException {
+        int update;
+        update = queryStatement.executeUpdate();
+        return update;
     }
 }
