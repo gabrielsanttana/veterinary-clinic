1322ec80e24417e2f2537f4441a4c8d999eecd6a
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-05-28 22:15:41 -0300

    fix

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,16 +5,12 @@ import java.util.Observer;
 import model.*;
 
 public class Controller {
-    public static void setObserver(Observer observer){
-        ClienteDAO.getInstance().addObserver(observer);
-    }
-    
     public static void addCliente(String nome, String endereco, String telefone, String cep, String email){
-        ClienteDAO.getInstance().addCliente(nome, endereco, telefone, cep, email);
+        ClientDAO.getInstance().addClient(nome, endereco, telefone, cep, email);
     }
     
     public static ArrayList<Client> getAllClientes(){
-        return new ArrayList<Client>(ClienteDAO.getInstance().getAllClientes().values());
+        return new ArrayList<Client>(ClientDAO.getInstance().getAllClients().values());
     }
 
     public static void addAnimal(String name, int age, String sex, Treatment[] treatments, Species species) {
